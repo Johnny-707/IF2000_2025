e8cd4701c088f89db48a4592c6b7111f038461a0
xiomara nuñez umaña <johnnyfranco73@gmail.com>
2025-08-14 11:56:00 -0600

    Prueba 1

---
+++ IF2000_introduccionprogra/src/main/java/com/mycompany/if2000_introduccionprogra/IF_2000_introduccion_progra.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.if2000_introduccionprogra;
+
+/**
+ *
+ * @author xio_d
+ */
+public class IF_2000_introduccion_progra {
+    
+
+    public static void main(String[] args) {
+        
+        //Data types
+        String name = "Lionel";
+        int age = 40;
+        
+         System.out.println("Hello World");
+        System.out.println("Name: " +name);
+        System.out.println("Age: "+age);
+        /*
+        Tipos de datos numericos:
+        int,long,short,float,double,numeric,money,decimal
+        
+        
+        manejo de fechas:
+        Date 
+        
+        Cadenas de caracteres:
+        String,char
+        
+        objetos:
+        object
+        
+        Data Structures:
+        Arrays or Vectors: int[] x;
+                           int[][] y;
+        
+        True/False
+        boolean
+        
+        
+        */
+        
+        
+   
+        
+       
+    }
+}
