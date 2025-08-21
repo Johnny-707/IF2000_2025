1dbe49d9bb4c31911ddf6c70405731431ece223c
xiomara nuñez umaña <johnnyfranco73@gmail.com>
2025-08-21 10:45:43 -0600

    Agrego paquete logic y actualizo clase principal

--- IF2000_introduccionprogra/src/main/java/com/mycompany/if2000_introduccionprogra/IF2000_introduccionprogra.java
+++ IF2000_introduccionprogra/src/main/java/com/mycompany/if2000_introduccionprogra/IF2000_introduccionprogra.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.if2000_introduccionprogra;
 
-/**
- *
- * @author xio_d
- */
+
 public class IF2000_introduccionprogra {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       // System.out.println("Hello World!");
     }
 }
---
+++ IF2000_introduccionprogra/src/main/java/logic/Ejercicios_semana2_PracticaEnClase.java
@@ -0,0 +1,20 @@
+
+package logic;
+
+
+public class Ejercicios_semana2_PracticaEnClase {
+
+    public Ejercicios_semana2_PracticaEnClase() {
+    }
+ 
+   public void ejercicioA(){
+       
+       
+       
+       
+   }    
+    
+    
+    
+    
+}
